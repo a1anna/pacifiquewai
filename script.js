712849321027147d1017e2bfe32e433ad0d63648
a1anna <alanna.matavao@gmail.com>
2026-07-20 19:16:54 -1000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.primary-nav');
-
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
